021660a6102dee67800130c78740ee9aee8ac099
parkchanghyup <ariz1623@naver.com>
2022-08-08 22:15:10 +0900

    테스트코드 예제

---
+++ 테스트 코드 작성하기/테스트코드 예제/tests/test_grabstore.py
@@ -0,0 +1,26 @@
+from main import GrabStore, Product
+
+# Unit test
+def test_show_product(grab_store):
+    # given
+
+
+    product_id = 1
+
+    #when
+    product = grab_store.show_product(product_id = product_id)
+
+    #then
+    assert product == Product(name = '키보드', price = 30000)
+
+# Intergration Test
+def test_sell_product_well(grab_store):
+    product_id = 1
+    pre_money = grab_store._money
+    product = grab_store.show_product(product_id = product_id)
+
+    _product = grab_store.sell_product(product_id=product_id, money=product.price)
+
+    assert grab_store._money == product.price
+    assert not grab_store.show_product(product_id=product_id)
+
