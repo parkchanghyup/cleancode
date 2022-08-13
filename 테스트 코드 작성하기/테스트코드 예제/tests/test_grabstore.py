0f98af55893347e9e93feee541b064c189509430
parkchanghyup <57655448+parkchanghyup@users.noreply.github.com>
2022-08-13 23:53:08 +0900

    테스트코드 예제

--- 테스트 코드 작성하기/테스트코드 예제/tests/test_grabstore.py
+++ 테스트 코드 작성하기/테스트코드 예제/tests/test_grabstore.py
@@ -1,25 +1,62 @@
-from main import GrabStore, Product
+import pytest
 
-# Unit test
-def test_show_product(grab_store):
-    # given
+from main import Product
+from unittest import mock
 
+# Unit Test
+from tests.conftest import API_URL
+
+
+def test_show_product(mock_api, grab_store, mock_products):
+    #given
     product_id = 1
+    mock_product = mock_products[product_id]
 
-    # when
+    #when
     product = grab_store.show_product(product_id=product_id)
 
-    # then
-    assert product == Product(name="키보드", price=30000)
+    #then
+    assert product == Product(name=mock_product["title"], price=mock_product["price"])
 
-
-# Intergration Test
-def test_sell_product_well(grab_store):
-    product_id = 1
+def test_take_money(grab_store):
+    price = 100
     pre_money = grab_store._money
-    product = grab_store.show_product(product_id=product_id)
 
+    grab_store._take_money(money=price)
+
+    assert grab_store._money == pre_money + price
+
+def test_return_money(grab_store):
+    price = 100
+    pre_money = grab_store._money
+
+    grab_store._return_money(money=price)
+
+    assert grab_store._money == pre_money - price
+
+
+def test_take_out_product(mock_api, grab_store, mock_products):
+    product_id = 1
+    mock_product = mock_products[product_id]
+
+    product = grab_store._take_out_product(product_id=1)
+
+    assert product == Product(name=mock_product["title"], price=mock_product["price"])
+    # assert not grab_store._products.get(product_id, None)
+
+# Integration Test
+def test_sell_product_well(mock_api, grab_store, mock_products):
+    product_id = 1
+    # pre_money = grab_store._money
+
+    product = grab_store.show_product(product_id=product_id)
     _product = grab_store.sell_product(product_id=product_id, money=product.price)
 
     assert grab_store._money == product.price
-    assert not grab_store.show_product(product_id=product_id)
+    # assert not grab_store.show_product(product_id=product_id)
+
+def test_sell_product_not_found(mock_api, grab_store, mock_products):
+    product_id = 100
+
+    with pytest.raises(Exception):
+        grab_store.sell_product(product_id=product_id, money=0)
